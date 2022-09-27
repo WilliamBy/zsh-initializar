d120d7aa015829f6d9663d0953f37ceafc1f977f
w1_liamby <weiliang_bai@foxmail.com>
2022-09-27 23:23:37 +0800

    fix

--- run.sh
+++ run.sh
@@ -10,28 +10,29 @@ fi
 if [ -z "${zsh_path}" ];
 then
 	# install zsh
+	echo "installing zsh"
 	sudo apt update
 	sudo apt install zsh -y
 fi
-# Oh-My-Zsh
+echo "installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Starship
+echo "installing starship..."
 echo "y" | curl -sS https://starship.rs/install.sh | sh
 user_config="${HOME}/.config"
 if [ ! -e "${user_config}" ];
 then
 	mkdir "${user_config}"
 else [ -f "${user_config}" ];
-	echo "cannot create directory ~/.config, please check if there is a same name file"
+	echo "cannot create directory ~/.config, please check if there is a same name file."
 fi
 cat ./config/starship.toml > "${HOME}/.config/starship.toml"
 
-# set zshrc and aliases
+echo "set zshrc and aliases..."
 cat ./config/.zshrc > "${HOME}/.zshrc"
 cat ./config/.aliases > "${HOME}/.aliases"
 
-# change default login shell to zsh
+echo "change default login shell to zsh"
 sudo chsh -s /usr/bin/zsh
 echo "Success! You can now relogin to enjoy new shell!"
 echo "For more information refer to https://starship.rs and https://github.com/ohmyzsh/ohmyzsh/wiki"
